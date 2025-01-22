373ee0303f83a56080b367f9416f8bfc1174f07f
Baran Yeni <info@baranyeni.com>
2025-01-22 05:17:26 +0300

    Fill seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require_relative '../app/services/orders/create_from_cart'
+
+customers = [
+  { first_name: 'John', last_name: 'Doe', email: 'john.doe@example.com', phone_number: '+123 456 7899' },
+  { first_name: 'Jane', last_name: 'Smith', email: 'jane.smith@example.com', phone_number: '+90 876 543 5214' },
+  { first_name: 'Alice', last_name: 'Johnson', email: 'alice.johnson@example.com', phone_number: '(122) 334 3455' }
+]
+
+customers.each do |customer_data|
+  Customer.create!(customer_data)
+end
+
+[
+  { name: 'Product A', price: 10.0, stock_count: 100, description: 'Product A description' },
+  { name: 'Product B', price: 20.0, stock_count: 50, description: 'Product B description' },
+  { name: 'Product C', price: 15.0, stock_count: 75, description: 'Product C description' },
+  { name: 'Product D', price: 25.0, stock_count: 25, description: 'Product D description' },
+  { name: 'Product E', price: 30.0, stock_count: 10, description: 'Product E description' },
+  { name: 'Product F', price: 35.0, stock_count: 5, description: 'Product F description' }
+].each do |product_data|
+  Product.create!(product_data)
+end
+
+Customer.all.each do |customer|
+  cart = Cart.create!(customer: customer)
+
+  cart_items = [
+    { product: Product.find_by(name: 'Product A'), quantity: 2 },
+    { product: Product.find_by(name: 'Product B'), quantity: 1 }
+  ]
+
+  cart_items.each do |cart_item_data|
+    cart.cart_items.create!(cart_item_data)
+  end
+end
+
+Orders::CreateFromCart.new(Cart.all.sample).call
+
+puts "Seed data created successfully!"
